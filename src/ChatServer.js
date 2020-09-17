22798f29694bd4a6e4cdb28e09ad361140d7ec03
Jacob Scheetz <scheetzj2@udayton.edu>
2020-09-17 21:18:26 +0000

    adding chatserver.js

--- src/ChatServer.js
+++ src/ChatServer.js
@@ -4,7 +4,7 @@ var server = http.createServer(app)
 const port = process.env.PORT || 8080
 server.listen(port);
 console.log(`Express HTTP Server is listening on port ${port}`)
-app.get('/', (request, reponse) => {
+app.get('/', (request, response) => {
     console.log("Got HTTP request")
     response.sendFile(__dirname+'/index.html')
 })
