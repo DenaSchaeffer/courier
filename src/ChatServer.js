796e164239548fe9fcb246eb3a5de2ddb4be35ba
Justen Stall <justenstall@gmail.com>
2020-09-17 21:46:59 +0000

    print client id

--- src/ChatServer.js
+++ src/ChatServer.js
@@ -7,7 +7,10 @@ console.log(`Express HTTP Server is listening on port ${port}`)
 app.get('/', (request, response) => {
     console.log("Got HTTP request")
     response.sendFile(__dirname+'/index.html')
-})
+});
 var io = require('socket.io');
-var socketio=io.listen(server);
-console.log("Socket.IO is listening at port: port")
+var socketio = io.listen(server);
+console.log("Socket.IO is listening at port: " + port);
+socketio.on("connection", function (socketclient) {
+    console.log("A new Socket.IO client is connected. ID= " + socketclient.id)
+});
