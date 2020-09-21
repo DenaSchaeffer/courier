40bb266002d56c38d820e0796d941172e5c86097
Justen Stall <justenstall@gmail.com>
2020-09-21 23:12:54 +0000

    fixing socket functions

--- src/ChatServer.js
+++ src/ChatServer.js
@@ -13,15 +13,15 @@ var socketio = io.listen(server);
 console.log("Socket.IO is listening at port: " + port);
 socketio.on("connection", function (socketclient) {
     console.log("A new Socket.IO client is connected. ID= " + socketclient.id)
-});
-socketclient.on("login", (username) => {
-    socketclient.username = username;
-    var welcomemessage = username + "has joined the chat system!";
-    console.log(welcomemessage);
-    socketio.sockets.emit("welcome", welcomemessage);
-});
-socketclient.on("chat", (message) => {
-    var chatmessage = socketclient.username + " says: " + message;
-    console.log(chatmessage);
-    socketio.sockets.emit("chat", chatmessage);
+    socketclient.on("login", (username) => {
+        socketclient.username = username;
+        var welcomemessage = username + "has joined the chat system!";
+        console.log(welcomemessage);
+        socketio.sockets.emit("welcome", welcomemessage);
+    });
+    socketclient.on("chat", (message) => {
+        var chatmessage = socketclient.username + " says: " + message;
+        console.log(chatmessage);
+        socketio.sockets.emit("chat", chatmessage);
+    });
 });
